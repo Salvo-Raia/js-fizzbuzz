cddb7e4068363056b291126fca1c61c69d218026
Salvo-Raia <salvo.raia@outlook.it>
2025-12-17 16:29:01 +0100

    Add Fizz and buzz for 3 and 5 multiples

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 console.log("JS Ready to go");
 
-/* Scrivi un programma che stampi i numeri da 1 a 100 */ 
+// Scrivi un programma che stampi i numeri da 1 a 100 
 for (let i = 1; i <= 100; i++ ) {
-    console.log(i);
-    
+
+/* ma per i multipli di 3 stampi “Fizz” al posto del numero e 
+   per i multipli di 5 stampi Buzz.
+   Per i numeri che sono sia multipli di 3 che di 5 stampi 
+   FizzBuzz. */
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("🥴 FizzBuzz");
+    }
+
+    else if (i % 5 == 0) {
+        console.log("🤪 Buzz");
+    } 
+
+    else if (i % 3 == 0)  {
+        console.log("😜​ Fizz");
+    }
+
+    else {
+        console.log(i);  
+    }
 }
 
